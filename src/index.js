aaba6ae421eaa4fd931498b818cfa0e7909a5be1
Angela Castle <eyesofthegoddess@hotmail.com>
2026-08-03 16:24:15 -0700

    starting up React Library Project

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faBars, faSHoppingCart } from '@fortawesome/free-solid-svg-icons'
+
+library.add(faBars, faSHoppingCart)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
