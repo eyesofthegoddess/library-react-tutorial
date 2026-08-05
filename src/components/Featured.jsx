068d2bde3128e286fc879011adf76f31efe22de5
Angela Castle <eyesofthegoddess@hotmail.com>
2026-08-05 09:45:00 -0700

    setting up Featured Books

---
+++ src/components/Featured.jsx
@@ -0,0 +1,25 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React from 'react';
+import Book from './ui/Book';
+
+const Featured = () => {
+    return (
+        <section id="features">
+            <div className="container">
+                <div className="row">
+                    <h2 className="section__title">
+                        Featured <span className="purple">Books</span>
+                    </h2>
+                    <div className="books">
+                        <Book />
+                        <Book />
+                        <Book />
+                        <Book />
+                    </div>
+                </div>
+             </div>
+        </section>
+    );
+}
+
+export default Featured;
