77906ead3e846b6f1205d4f2afca8d38c3ca8949
Angela Castle <eyesofthegoddess@hotmail.com>
2026-08-07 15:45:26 -0700

    finished sorting options

--- src/pages/Books.jsx
+++ src/pages/Books.jsx
@@ -8,15 +8,15 @@ const Books = ({ books: initialBooks }) => {
         console.log(filter);
         if (filter === "LOW_TO_HIGH") {
             setBooks(books.slice().sort((a, b) => 
-                            a.salePrice || a.originalPrice - 
-                            b.salePrice || b.originalPrice
+                            (a.salePrice || a.originalPrice) - 
+                            (b.salePrice || b.originalPrice)
                     )
             );
         }
         if (filter === "HIGH_TO_LOW") {
             setBooks(books.slice().sort((a, b) => 
-                            b.salePrice || b.originalPrice - 
-                            a.salePrice || a.originalPrice
+                            (b.salePrice || b.originalPrice) - 
+                            (a.salePrice || a.originalPrice)
                     )
             );
         }
@@ -35,8 +35,8 @@ const Books = ({ books: initialBooks }) => {
                             <h2 className="section__title books__header--title">All Books</h2>
                             <select id="filter" defaultValue="DEFAULT" onChange={(event) => filterBooks(event.target.value)} >
                                 <option value="DEFAULT" disabled>Sort</option>
-                                <option value="LOW TO HIGH">Price, Low to High</option>
-                                <option value="HIGH TO LOW">Price, High to Low</option>
+                                <option value="LOW_TO_HIGH">Price, Low to High</option>
+                                <option value="HIGH_TO_LOW">Price, High to Low</option>
                                 <option value="RATING">Rating</option>
                             </select>
                         </div>
