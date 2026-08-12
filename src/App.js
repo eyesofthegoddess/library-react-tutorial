55c8fb2c0a698b392d4b8cdbd14d9e4caca65e41
Angela Castle <eyesofthegoddess@hotmail.com>
2026-08-11 17:12:23 -0700

    finished dynamics and started new seciton recommended

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
 import Footer from './components/Footer';
 import Nav from './components/Nav';
-import { BrowserRouter as Router, Route, } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Home from './pages/Home';
 import Books from './pages/Books';
 import { books } from './data';
+import BookInfo from './pages/BookInfo';
 
 function App() {
   return (
     <Router>
-    <div className="App">
-      <Nav />
-      <Route path="/" exact component={Home} />
-      <Route path="/books" render={() => <Books books={books} />} />
-      <Footer />
-    </div>
+      <div className="App">
+        <Nav />
+        <Routes>
+          <Route path="/" exact element={<Home />} />
+          <Route path="/books" exact element={<Books books={books} />} />
+          <Route path="/books/:id" element={<BookInfo books={books} />} />
+        </Routes>
+        <Footer />
+      </div>
     </Router>
   );
 }
