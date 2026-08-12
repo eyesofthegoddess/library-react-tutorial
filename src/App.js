7223a3880128cbfe527d47f8f492b116628690b9
Angela Castle <eyesofthegoddess@hotmail.com>
2026-08-12 15:42:33 -0700

    working through cart

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
 import Footer from './components/Footer';
+import React, { useState, useEffect } from "react"
 import Nav from './components/Nav';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Home from './pages/Home';
 import Books from './pages/Books';
 import { books } from './data';
 import BookInfo from './pages/BookInfo';
+import Cart from './pages/Cart';
 
 function App() {
+  const [cart, setCart] = useState([]);
+
+  function addToCart(book) {
+    const dupeItem = cart.find(item => +item.id === +book.id)
+    if (dupeItem) {
+      dupeItem.quantity += 1;
+    }
+    setCart([...cart, {...book, quantity: 1}])
+  }
+
+  useEffect(() => {
+    console.log(cart)
+  }, [cart])
+
   return (
     <Router>
       <div className="App">
@@ -14,7 +30,8 @@ function App() {
         <Routes>
           <Route path="/" exact element={<Home />} />
           <Route path="/books" exact element={<Books books={books} />} />
-          <Route path="/books/:id" element={<BookInfo books={books} />} />
+          <Route path="/books/:id" element={<BookInfo books={books} addToCart={addToCart} />} />
+          <Route path="/cart" element={<Cart books={books} />} />
         </Routes>
         <Footer />
       </div>
--- src/pages/BookInfo.jsx
+++ src/pages/BookInfo.jsx
@@ -3,10 +3,11 @@ import React from 'react'
 import { Link, useParams } from 'react-router-dom';
 import Rating from '../components/ui/Rating';
 import Price from '../components/ui/Price';
+import Book from '../components/ui/Book';
 
-const BookInfo = ({ books }) => {
+const BookInfo = ({ books, addToCart }) => {
   const { id } = useParams();
-  const book = books.find(book => book.id === id);
+  const book = books.find(book => +book.id === +id);
   console.log(book)
   return (
     <div id="books__body">
@@ -23,28 +24,28 @@ const BookInfo = ({ books }) => {
           </div>
           <div className="book__selected">
             <figure className="book__selected--figure">
-              <img src="https://m.media-amazon.com/images/I/61mIq2iJUXL._AC_UF1000,1000_QL80_.jpg" alt="" className="book__selected--img" />
+              <img src={book.url} alt="" className="book__selected--img" />
             </figure>
             <div className="book__selected--description">
-              <h2 className="book__selected--title">Crack the coding interview</h2>
-              <Rating rating="4.5" />
+              <h2 className="book__selected--title">
+                {book.title}
+                </h2>
+              <Rating rating={book.rating} />
               <div className="book__selected--price">
-              <Price originalPrice={50} salePrice={20} />
+              <Price originalPrice={book.originalPrice} salePrice={book.salePrice} />
               </div>
               <div className="book__summary">
-                <div className="book__summary--title">
+                <h3 className="book__summary--title">
                   Summary
-                </div>
-                <p className="book__summary__para">
+                </h3>
+                <p className="book__summary--para">
                   Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam, accusamus assumenda deleniti obcaecati, quisquam quis laboriosam inventore, tempora voluptate suscipit non rerum veniam sint. A labore deleniti sed? Porro, veniam.
                 </p>
-                                <p className="book__summary__para">
+                                <p className="book__summary--para">
                   Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam, accusamus assumenda deleniti obcaecati, quisquam quis laboriosam inventore, tempora voluptate suscipit non rerum veniam sint. A labore deleniti sed? Porro, veniam.
                 </p>
               </div>
-              <button className="btn">
-                Add to cart
-              </button>
+              <button className="btn" onClick={() => addToCart(book)}>Add to cart</button>
             </div>
           </div>
         </div>
@@ -55,6 +56,13 @@ const BookInfo = ({ books }) => {
             <h2 className="book__selected--title--top">
               Recommended Books
             </h2>
+          </div>
+          <div className="books">
+          {books
+          .filter(book => book.rating === 5 && +book.id !== +id)
+          .slice(0, 4)
+          .map(book => <Book book={book} key={book.id} />)
+          }
           </div>
         </div>
       </div>
